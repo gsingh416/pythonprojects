a5c35622845a52e02d90bcd0b38238af0d15cd83
gsingh41 <gaurav.u.singh@capgemini.com>
2023-11-08 09:46:51 +0000

    roll dice fix apply

--- rolldice.py
+++ rolldice.py
@@ -5,7 +5,7 @@ import random
 def roll_dice(min_val, max_val):
   while True:
     print("Dice Rolling...")
-    print(f"Your number is {random.randint(min_val, max_val)}")
+    #print(f"Your number is {random.randint(min_val, max_val)}")
     result = input("Do you want to roll the dices again? (y/n) ")
     if result.lower() == "n":
       break
